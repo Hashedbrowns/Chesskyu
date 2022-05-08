169677076d20762c3bc8566a86451c02952f8db4
Ashwin Shreekumar <ashwinshreekumar123@gmail.com>
2022-05-07 20:39:12 -0600

    Added piece images to board

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 const chess = new Chess()
-let board = new Chessboard('board', 'start')
+
+let boardRepr = chess.board()
+const board = document.querySelector(".board")
+let img;
+let piece;
+let coordinates;
+console.log(boardRepr)
+for (let row = 7; row >= 0; row--) {   
+    for (let column = 0; column < 8; column++) {
+        if (boardRepr[row][column]) {
+            piece = `pieces/${boardRepr[7-row][column].color}${boardRepr[7-row][column].type}.svg`;
+            coordinates = `#${String.fromCharCode(97+column)}${row+1}`;
+            console.log(coordinates,piece)
+            coordinates = document.querySelector(coordinates)
+            img = document.createElement("img");
+            img.src = piece;
+            coordinates.appendChild(img);
+        }
+    }
+}
