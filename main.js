5278ab6a7e474bacb7db0abac05df867455ec8c3
Ashwin Shreekumar <ashwinshreekumar123@gmail.com>
2022-05-07 21:21:26 -0600

    Fixed piece size in square

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ const board = document.querySelector(".board")
 let img;
 let piece;
 let coordinates;
-console.log(boardRepr)
+
 for (let row = 7; row >= 0; row--) {   
     for (let column = 0; column < 8; column++) {
         if (boardRepr[row][column]) {
             piece = `pieces/${boardRepr[7-row][column].color}${boardRepr[7-row][column].type}.svg`;
             coordinates = `#${String.fromCharCode(97+column)}${row+1}`;
-            console.log(coordinates,piece)
             coordinates = document.querySelector(coordinates)
             img = document.createElement("img");
             img.src = piece;
